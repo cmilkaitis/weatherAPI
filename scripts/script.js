3718f106a76034c3a045a94cd608af68a8172c08
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-19 11:24:12 -0400

    v1 working

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,27 @@
 $(document).ready(function(){
 
     if(navigator.geolocation){
-      
-        function sendData(lat, long){
-            $("#data").html("<h2>Latitude: </h2>" + lat + "<br><h2>Longitude: </h2>" + long);
-        };
 
         navigator.geolocation.getCurrentPosition(function(position){
-            //sendData(position.coords.latitude, position.coords.longitude);
-
+           
             let lon = position.coords.longitude;
             let lat = position.coords.latitude;
             let url = 'https://fcc-weather-api.glitch.me/api/current?lon=' + lon + '&lat='+ lat;
 
             $.getJSON(url, function(json){
-                let html = "xx";
+            
+                let s = JSON.stringify(json);
+                let obj = JSON.parse(s);
+                console.log(obj.name);
+                console.log(obj.main.temp);
+                console.log(obj);
 
-                json.forEach(val => {
-                    let keys = Object.keys(val);
-                    html += "<div class='info'>";
-                    keys.forEach(key => {
-                        html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
-                    });
-                    html += "</div><br>"
-                });
+                function toFar(num) {
+                    return (num * 1.8) + 32;
+                }
 
-                $("#data").html(html);
+                $("#data").html("City: " + obj.name + "<br>Temp: " + obj.main.temp + "<br>" + obj.weather[0].main);
+                
             }); 
         });
     } else {
